b1423402a635819b5676b07a8af3aee2adce0880
milrosen <61394098+milrosen@users.noreply.github.com>
2023-07-24 13:07:40 -0400

    initial commit 🚀🍆💦

---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import PocketBase from 'pocketbase';
+
+	const pb = new PocketBase('http://127.0.0.1:8090');
+
+	async function startPocketBase() {
+		const result = await pb.collection('test').getFullList();
+		return result[0].test_field;
+	}
+
+	let data = startPocketBase();
+</script>
+
+{#await data}
+	<h1>..loading</h1>
+{:then data}
+	<h1>{data}</h1>
+{:catch}
+	<h1>theres been an error, mad cuz bad, skill issue</h1>
+{/await}
