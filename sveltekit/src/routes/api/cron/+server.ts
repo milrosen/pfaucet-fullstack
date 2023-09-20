e73ef217ab7383994a8e82f346a5da5c8832cc29
milrosen <61394098+milrosen@users.noreply.github.com>
2023-09-20 19:06:25 -0400

    we do a little debugging in production

--- sveltekit/src/routes/api/cron/+server.ts
+++ sveltekit/src/routes/api/cron/+server.ts
@@ -1,6 +1,10 @@
 import { json } from "@sveltejs/kit";
 
 export async function GET(request) {
+	const requestFromVercel = request.url.toString().match(/https:\/\/pfaucet-fullstack-[a-zA-Z0-9]+-miltonrosenbaum-mcgillca.vercel.app\/api\/cron/)
+	if (!requestFromVercel) {
+		return new Response('not from vercel', {status: 403});
+	}
 	console.log(JSON.stringify(request))
-	return json({ request });
+	return new Response('yay!', {status: 200});
 }
